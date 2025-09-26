b7705fd1ac32c1950fcb6da8ab3a65942e3cacc3
Vaghela_Jenil <jenilvaghela9008@gmail.com>
2025-09-27 01:03:41 +0530

    common commit

---
+++ components/Header.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { SignedOut, SignedIn, SignInButton, UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from './ui/button'
+import { LayoutDashboard, PenBox } from 'lucide-react'
+import { checkUser } from '@/lib/CheckUser'
+const Header = async () => {
+  await checkUser();
+  return (
+
+    <div className='fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b'>
+
+      <nav className="container mx-auto py-4 px-4 flex items-center justify-between">
+        <Link href='/'>
+          <Image
+            className='h-12 w-auto object-contain'
+            src={'/logo.webp'} alt='logo' height={60} width={200} />
+        </Link>
+
+
+        <div className='flex items-center space-x-4'>
+            <SignedIn>
+              <Link href={'/dashboard'} className='text-gray-600 hover:text-blue-600 flex items-center gap-2'>
+              <Button variant={'outline'}>
+                  <LayoutDashboard size={18}/>
+                  <span className='hidden md:inline'>Dashboard</span>
+              </Button>
+              </Link>
+
+              <Link href={'/transection/create'} className='flex items-center gap-2'>
+              <Button >
+                  <PenBox size={18}/>
+                  <span className='hidden md:inline'>Transection</span>
+              </Button>
+              </Link>
+            </SignedIn>
+
+          <SignedOut>
+            <SignInButton forceRedirectUrl='/dashboard' >
+            <Button variant='outline'>Login</Button>
+            </SignInButton>
+            
+          </SignedOut>
+          <SignedIn>
+            <UserButton  appearance={{
+              elements:{
+                avatarBox:'w-10 h-10'
+              }
+            }}/>
+          </SignedIn>
+        </div>
+      </nav>
+    </div>
+
+  )
+}
+
+export default Header
---
+++ components/Hero.jsx
@@ -0,0 +1,60 @@
+"use client";
+import Link from 'next/link'
+import React, { useEffect, useRef } from 'react'
+import { Button } from './ui/button'
+import Image from 'next/image'
+
+
+const HeroSection = () => {
+
+  const imageRef = useRef();
+
+  useEffect(() =>{
+    const imageElement = imageRef.current;
+
+    const handleScroll = () =>{
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 100;
+
+      if(scrollPosition > scrollThreshold){
+        imageElement.classList.add('scrolled');
+      }else{
+        imageElement.classList.remove('scrolled');
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll)
+  },[])
+
+  return (
+    <div className='pb-20 px-4'>
+      <div className='pb-20 px-4'>
+        <div className='container mx-auto text-center'>
+          <h1 className='text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title'>
+            Manage Your Finances <br /> with Intelligence
+          </h1>
+          <p className='text-xl text-gray-600 mb-8 max-w-2xl mx-auto'>
+            An AI-powered financial management platform that helps you track
+            analyze, and optimize your spending with real-time insights.
+          </p>
+          <div className='flex justify-center space-x-4'>
+            <Link href={'/dashboard'}>
+              <Button size='lg' className="px-8 ">Get Started</Button>
+            </Link>
+            <Link href={'https://drive.google.com/file/d/1leZyfMTZsJXuwfxF2TGY4DChZTszoriU/view?usp=sharing'}>
+              <Button variant={'outline'} size='lg' className="px-8 ">Watch demo</Button>
+            </Link>
+          </div> 
+        </div>
+      </div>
+      <div className='hero-image-wrapper'>
+        <div ref={imageRef} className='hero-image'>
+          <Image src={'/banner.png'} width={980} height={720} alt='Dashboard Preview' className='rounded-l-lg shadow-2xl border mx-auto' priority />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeroSection
